e8ce50420c0ea310f76ac3710f307e0b9e9f18b3
Krunal Dudhatra <krunaldudhatra04@gmail.com>
2025-07-05 16:39:17 +0530

    passed test case for supports custom delimiter syntax

--- Stringcalculator/src/stringCalc.js
+++ Stringcalculator/src/stringCalc.js
@@ -1,8 +1,20 @@
 function Add(numbers) {
   if (!numbers) return 0;
 
-  const parts = numbers.split(/[\n,]/);
-  return parts.reduce((sum, num) => sum + parseInt(num), 0);
+  let delimiters = /[\n,]/;
+  if (numbers.startsWith("//")) {
+    const [_, delim, rest] = numbers.match(/^\/\/(.+)\n(.*)$/);
+    delimiters = new RegExp(`[${escape(delim)}]`);
+    numbers = rest;
+  }
+
+  return numbers
+    .split(delimiters)
+    .reduce((sum, num) => sum + parseInt(num), 0);
+}
+
+function escape(str) {
+  return str.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
 }
 
 
