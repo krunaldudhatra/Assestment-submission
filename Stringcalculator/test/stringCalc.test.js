27dae17437160bf03995f5d146e1b8d22da4c2b2
Krunal Dudhatra <krunaldudhatra04@gmail.com>
2025-07-05 16:23:18 +0530

    failed test case for returns number for single value

--- Stringcalculator/test/stringCalc.test.js
+++ Stringcalculator/test/stringCalc.test.js
@@ -3,3 +3,8 @@ const { Add } = require('../src/stringCalc.js');
 test('returns 0 for empty string', () => {
   expect(Add("")).toBe(0);
 });
+
+test('returns number for single value', () => {
+  expect(Add("1")).toBe(1);
+});
+
