8db2d108b07a51f3ffbbe1b016f7f4af4c007848
Krunal Dudhatra <krunaldudhatra04@gmail.com>
2025-07-05 16:37:05 +0530

    passed test case for supports newline as delimiter

--- Stringcalculator/src/stringCalc.js
+++ Stringcalculator/src/stringCalc.js
@@ -1,9 +1,10 @@
 function Add(numbers) {
   if (!numbers) return 0;
 
-  const parts = numbers.split(',');
+  const parts = numbers.split(/[\n,]/);
   return parts.reduce((sum, num) => sum + parseInt(num), 0);
 }
+
 
 
 module.exports = { Add };
