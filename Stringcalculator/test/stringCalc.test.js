46fda3a15421ec6a4b760cefb9695eef96c3c328
Krunal Dudhatra <krunaldudhatra04@gmail.com>
2025-07-05 16:33:21 +0530

    returns sum of multiple comma-separated numbers

--- Stringcalculator/test/stringCalc.test.js
+++ Stringcalculator/test/stringCalc.test.js
@@ -11,3 +11,7 @@ test('returns number for single value', () => {
 test('returns sum of two comma-separated numbers', () => {
   expect(Add("1,2")).toBe(3);
 });
+
+test('returns sum of multiple comma-separated numbers', () => {
+  expect(Add("1,2,3,4")).toBe(10);
+});
